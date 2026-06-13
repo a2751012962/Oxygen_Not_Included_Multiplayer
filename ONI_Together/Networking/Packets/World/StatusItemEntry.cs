07709963d5640130670b5daa2a77fa4a55593fd8
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2026-06-13 17:37:05 +0100

    Restored status changes

--- ONI_Together/Networking/Packets/World/StatusItemEntry.cs
+++ ONI_Together/Networking/Packets/World/StatusItemEntry.cs
@@ -5,39 +5,29 @@ namespace ONI_Together.Networking.Packets.World
 {
     public struct StatusItemEntry
     {
-        public int ItemHash;
-        public int CategoryHash;
+        public string ItemId;
+        public string CategoryId;
         public string DisplayName;
         public string Tooltip;
 
         public void Serialize(BinaryWriter writer)
         {
             using var _ = Profiler.Scope();
-            writer.Write(ItemHash);
-            writer.Write(CategoryHash);
-
-            byte flags = 0;
-            if (DisplayName != null) flags |= 1;
-            if (Tooltip != null) flags |= 2;
-            writer.Write(flags);
-
-            if ((flags & 1) != 0) writer.Write(DisplayName);
-            if ((flags & 2) != 0) writer.Write(Tooltip);
+            writer.Write(ItemId ?? string.Empty);
+            writer.Write(CategoryId ?? string.Empty);
+            writer.Write(DisplayName ?? string.Empty);
+            writer.Write(Tooltip ?? string.Empty);
         }
 
         public static StatusItemEntry Deserialize(BinaryReader reader)
         {
             using var _ = Profiler.Scope();
-            int itemHash = reader.ReadInt32();
-            int categoryHash = reader.ReadInt32();
-            byte flags = reader.ReadByte();
-
             return new StatusItemEntry
             {
-                ItemHash = itemHash,
-                CategoryHash = categoryHash,
-                DisplayName = (flags & 1) != 0 ? reader.ReadString() : null,
-                Tooltip = (flags & 2) != 0 ? reader.ReadString() : null,
+                ItemId = reader.ReadString(),
+                CategoryId = reader.ReadString(),
+                DisplayName = reader.ReadString(),
+                Tooltip = reader.ReadString(),
             };
         }
     }
